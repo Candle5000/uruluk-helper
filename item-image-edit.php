5fd86584348497ccc5bb2511a978f96f86ee8749
Candle5000 <candle5000@icloud.com>
2019-06-06 00:34:12 +0900

    アイテム画像加工ツールを作成

---
+++ item-image-edit.php
@@ -0,0 +1,32 @@
+<?php
+$pixelsX = [4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4];
+$pixelsY = [4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4];
+$startX = 250;
+$startY = 25;
+foreach (glob("img-src/*.png") as $filepath) {
+	$srcImg = imagecreatefrompng($filepath);
+	$outImg = imagecreatetruecolor(32, 32);
+	$srcX = $startX;
+	$srcY = $startY;
+	$x = 0;
+	$y = 0;
+	foreach ($pixelsX as $pixelX) {
+		foreach ($pixelsY as $pixelY) {
+			$rgb = imagecolorat($srcImg, $srcX, $srcY);
+			$r = ($rgb >> 16) & 0xFF;
+			$g = ($rgb >> 8) & 0xFF;
+			$b = $rgb & 0xFF;
+			$color = imagecolorallocate($outImg, $r, $g, $b);
+			imagefilledrectangle($outImg, $x, $y, $x, $y, $color);
+			$srcY += $pixelY;
+			$y++;
+		}
+		echo $srcX . "\n";
+		$srcX += $pixelX;
+		$x++;
+		$srcY = $startY;
+		$y = 0;
+	}
+	imagepng($outImg, "img-out/" . basename($filepath), 0);
+	imagedestroy($outImg);
+}
