1065422a8f7ffdc9fda0a27451c6d7b76cf4e286
Candle5000 <candle5000@icloud.com>
2019-07-14 21:06:03 +0900

    スクリーンショットのサイズに応じた設定を読み込むよう変更

--- item-image-edit.php
+++ item-image-edit.php
@@ -1,30 +1,35 @@
 <?php
-$pixelsX = [4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4];
-$pixelsY = [4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4];
-$startX = 250;
-$startY = 25;
+$resizeInfo = [
+	640 => [1136 => ['startX' => 250, 'startY' => 25, 'iconsize' => 142]],
+	750 => [1334 => ['startX' => 294, 'startY' => 30, 'iconsize' => 166]],
+	1125 => [2436 => ['startX' => 441, 'startY' => 146, 'iconsize' => 250]],
+];
 foreach (glob("img-src/*.png") as $filepath) {
+	$size = getimagesize($filepath);
+	$sizeX = $size[0];
+	$sizeY = $size[1];
+	echo "Input image:$filepath ($sizeX*$sizeY)\n";
+	if (!array_key_exists($sizeX, $resizeInfo)
+			|| !array_key_exists($sizeY, $resizeInfo[$sizeX])) {
+		echo "Setting undefined. " . $sizeX . "*" . $sizeY . "\n";
+		continue;
+	}
 	$srcImg = imagecreatefrompng($filepath);
 	$outImg = imagecreatetruecolor(32, 32);
-	$srcX = $startX;
-	$srcY = $startY;
-	$x = 0;
-	$y = 0;
-	foreach ($pixelsX as $pixelX) {
-		foreach ($pixelsY as $pixelY) {
+	$startX = $resizeInfo[$sizeX][$sizeY]['startX'];
+	$startY = $resizeInfo[$sizeX][$sizeY]['startY'];
+	$pixelLen = $resizeInfo[$sizeX][$sizeY]['iconsize'] / 32;
+	for ($i = 0; $i < 32; $i++) {
+		for ($j = 0; $j < 32; $j++) {
+			$srcX = round($startX + $pixelLen * $i);
+			$srcY = round($startY + $pixelLen * $j);
 			$rgb = imagecolorat($srcImg, $srcX, $srcY);
 			$r = ($rgb >> 16) & 0xFF;
 			$g = ($rgb >> 8) & 0xFF;
 			$b = $rgb & 0xFF;
 			$color = imagecolorallocate($outImg, $r, $g, $b);
-			imagefilledrectangle($outImg, $x, $y, $x, $y, $color);
-			$srcY += $pixelY;
-			$y++;
+			imagefilledrectangle($outImg, $i, $j, $i, $j, $color);
 		}
-		$srcX += $pixelX;
-		$x++;
-		$srcY = $startY;
-		$y = 0;
 	}
 	imagepng($outImg, "img-out/" . basename($filepath), 0);
 	imagedestroy($outImg);
